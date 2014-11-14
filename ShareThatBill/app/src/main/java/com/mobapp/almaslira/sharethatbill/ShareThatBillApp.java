84aca4a3620b7ce27a7232a7bd135f3654546bcd
Jose Almas <jeajjr@gmail.com>
2014-11-13 21:19:26 -0500

    fixup! Created ShareThatBillApp, fixed bugs and added functionality on tabs

---
+++ ShareThatBill/app/src/main/java/com/mobapp/almaslira/sharethatbill/ShareThatBillApp.java
@@ -0,0 +1,23 @@
+package com.mobapp.almaslira.sharethatbill;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by José Ernesto on 11/12/2014.
+ */
+public class ShareThatBillApp extends Application {
+	static final String TAG = "ShareThatBillApp";
+
+	public FakeDataBase dataBase;
+
+	@Override
+	public void onCreate() {
+		Log.d(TAG, "onCreate");
+
+		super.onCreate();
+
+		dataBase = new FakeDataBase();
+		dataBase.initialize();
+	}
+}
